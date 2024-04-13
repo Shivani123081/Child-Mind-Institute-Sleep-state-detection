3ea669ebab3bd886823780c6f9ac4573df99063d
shivani <151901851+Shivani123081@users.noreply.github.com>
2024-04-13 16:05:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ import streamlit_ext as ste
 st.markdown("<h1 style='text-align: center; color: black;'>Child Mind Institute challenge - sleep detection</h1>", unsafe_allow_html=True)
 #st.title("Child Mind Institute challenge - sleep detection")
 # image titre
-img_title = Image.open(os.path.join("input", "input_Health_Theme.jpg"))
+img_title = Image.open(os.path.join("input_Health_Theme.jpg"))
 st.image(img_title, caption="Source: mouser.in/blog/remote-sensing-fosters-healthier-lives")
 st.markdown("**Detection of sleep onset and wake from wrist-worn accelerometer data**:watch:")
 st.markdown('<div style="text-align: justify;">\
