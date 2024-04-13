49b823b76fef63592310db6aec41b0428d17d697
shivani <151901851+Shivani123081@users.noreply.github.com>
2024-04-13 14:49:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,35 +7,21 @@ import pyarrow
 import fastparquet
 import os
 import pickle
-
 from utils import *
-
-    
-
 import streamlit_ext as ste
-    
-
-
 st.markdown("<h1 style='text-align: center; color: black;'>Child Mind Institute challenge - sleep detection</h1>", unsafe_allow_html=True)
 #st.title("Child Mind Institute challenge - sleep detection")
-
 # image titre
 img_title = Image.open(os.path.join("input", "Health_Theme.jpg"))
 st.image(img_title, caption="Source: mouser.in/blog/remote-sensing-fosters-healthier-lives")
-
 st.markdown("**Detection of sleep onset and wake from wrist-worn accelerometer data**:watch:")
-
 st.markdown('<div style="text-align: justify;">\
             The accelerometer data consists of 2 features, enmo and anglez.<br>\
             The time difference between records is 5 seconds.<br>\
             <strong>Enmo</strong>: the Euclidean Norm Minus One of all accelerometer signals, with negative values rounded to zero.<br>\
             <strong>Anglez</strong>:  a metric derived from individual accelerometer components that refers to the angle of the arm relative to the vertical axis of the body.<br><br></div>',\
                 unsafe_allow_html=True)
-
-
 st.divider()
-
-
 # logos kaggle and CMI
 img_logos = Image.open(os.path.join("input", "Kaggle_CMI.png"))
 col1, col2, col3 = st.columns([1.2, 5, 1.2])
@@ -45,108 +31,70 @@ col2.image(img_logos, use_column_width=True)
 with col2:
     st.write("Kaggle competition [main page](https://www.kaggle.com/competitions/child-mind-institute-detect-sleep-states), competition host: [Child Mind Institute](https://childmind.org/)", use_column_width=True)
 st.divider()
-
-
 st.header("Example:mag_right:")
 st.markdown("For one series (records of one accelerometer on several days) we plot the features, then the labelled target and our predictions with an XGBoost Classifier.")
-
 st.markdown('Visualization of the features of the series:')
 img1 = Image.open(os.path.join("input", "enmo_anglez.png"))
 st.image(img1)
-
 st.markdown('Visualization of enmo with sleep periods:zzz: (given by kaggle):')
 img2 = Image.open(os.path.join("input", "enmo_target.png"))
 st.image(img2)
-
 st.markdown('Visualization of enmo with sleep periods and model predictions:')
 img3 = Image.open(os.path.join("input", "enmo_target_prediction.png"))
 st.image(img3)
-
-
-
 line_break()
-
-
-
 st.header("Try it out:rocket:")
 file = st.file_uploader("Upload data", type=["csv", "parquet"])
 st.markdown("Display a random series from your data without and with predictions. Then you can download the prediction file for all series.")
-
 if file is None:
     st.text("Waiting for upload")
 else:
     with st.spinner("Checking file..."):
-
         # read file
         df = read_file(file.name, file)
-
         # check df has correct columns and types
         check = check_df(df)
     if check != True:
         st.write('Sorry, the data is not correct:')
         for elem in check:
             st.write(' - ', elem)
-        
-
     else:
-    
         with st.spinner("Plotting enmo..."):
             # get id for series we will plot
             id_viz = get_random_id(df)
-
             # plot enmo
-            st.pyplot(plot_enmo(df, id_viz))
-        
+            st.pyplot(plot_enmo(df, id_viz))        
         with st.spinner("Feature engineering..."):
-
             # feature engineering
             df_prepared = feature_engineering(df)
-
         with st.spinner("Loading pipeline..."):
-
             # load pipeline
             fp_pipeline = os.path.join("input", "pipeline_01.pkl")
             pipeline_1 = pickle.load(open(fp_pipeline, 'rb'))
-
         with st.spinner("Predictions..."):
             # get predictions
             y_pred, y_probas = get_predictions_probas(df_prepared, pipeline_1)
-
         with st.spinner ("Building submission dataframe..."):
             df_prediction = build_submission(df_prepared, y_pred, y_probas)
-
         with st.spinner("Plotting predictions..."):
             st.pyplot(plot_prediction(df_prepared, df_prediction, id_viz))
-
-        
         # display prediction dataset
         st.write("Dataframe of predicted events:")
         st.dataframe(df_prediction.drop(columns=["row_id"]))
         st.markdown("The score column is the confidence of the model in the prediction.")
-
         with st.spinner("Creating prediction file..."):
-            prediction_file = df_prediction.to_csv().encode('utf-8')
-
-
+            prediction_file = df_prediction.to_csv().encode('utf-8'
         ste.download_button(
             label="Download prediction file",
             data=prediction_file,
             file_name="predictions.csv",
             mime='text/csv'
             )
-        
-
-
 line_break()
-
-
-
 st.header("No data? Try sample series:sparkles:")
 selected_sample = st.selectbox(
     'Select a sample:',
     ('-', 'Sample 1', 'Sample 2', 'Sample 3'))
-
-
 dict_choice = {'Sample 1': os.path.join("input", "series_1.csv"),
                'Sample 2': os.path.join("input", "series_2.csv"),
                'Sample 3': os.path.join("input", "series_3.csv")
@@ -154,7 +102,6 @@ dict_choice = {'Sample 1': os.path.join("input", "series_1.csv"),
 selected_series = dict_choice.get(selected_sample, None)
 if selected_series != None:
     with st.spinner("Loading data..."):
-
         # read file
         df_2 = pd.read_csv(selected_series)
 
